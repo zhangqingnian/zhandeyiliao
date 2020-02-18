438c808f3016bc23ee968f72f0be8f54af389427
zhangqingnian <960294873@qq.com>
2020-02-18 17:03:46 +0800

    2.18.3

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,13 +1,8 @@
 
 module.exports = {
-<<<<<<< HEAD
     // baseURL: 'http://192.168.1.88:8081/',
     //baseURL: 'http://dingt.natapp1.cc/',
     
     //baseURL: 'http://lfsj.nat300.top/'
-=======
-    //baseURL: 'http://192.168.1.78:8081/',
-    //baseURL: 'http://dingt.natapp1.cc/',
->>>>>>> cbc118948b970655b8fa7322427de5cb76f4bae7
     baseURL: 'http://zhande.xisheninfo.com/',
 }
